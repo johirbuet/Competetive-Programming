0ea8d8e709acf0e7b2b42a515905d59b0b951a5b
Md Johirul Islam <Md Johirul Islam>
2017-04-29 11:36:21 -0500

    MEdians

--- src/UVA_10347.java
+++ src/UVA_10347.java
@@ -1,26 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class UVA_10347 {
 
-	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String m = "";
-        while ((m = br.readLine()) != null) {
-            StringTokenizer st = new StringTokenizer(m);
-            double m1 = Double.parseDouble(st.nextToken());
-            double m2 = Double.parseDouble(st.nextToken());
-            double m3 = Double.parseDouble(st.nextToken());
-            double med = (m1 + m2 + m3) / 2.0;
-            double area = (4 / 3.0) * Math.sqrt(med * (med - m1) * (med - m2) * (med - m3));
-            if (!(area > 0)) {
-                area = -1;
-            }
-            System.out.printf("%.3f\n", area);
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		while(sc.hasNextLine())
+		{
+			String line =sc.nextLine();
+			if(line == null || line.isEmpty())
+			{
+				break;
+			}
+			StringTokenizer st =new StringTokenizer(line);
+			double m1= Double.parseDouble(st.nextToken());
+			double m2 =Double.parseDouble(st.nextToken());
+			double m3 = Double.parseDouble(st.nextToken());
+			double sm =(m1+m2+m3)/2.0;
+			double a_sq = sm*(sm-m1)*(sm-m2)*(sm-m3);
+			if(a_sq<=0)
+			{
+				double f =-1.0;
+				System.out.printf("%.3f\n",f);
+			}
+			else
+			{
+				double s =Math.sqrt(a_sq)*(4/3.0);
+				System.out.printf("%.3f\n",s);
+			}
+		}
+	}
 
 }
